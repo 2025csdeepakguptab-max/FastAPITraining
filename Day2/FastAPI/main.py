568e8e7fe3cf6ee7d8c6b6360be71fae78f154fd
2025csdeepakguptab-max <2025cs_deepakgupta_b@nie.ac.in>
2026-09-22 13:08:47 +0530

    woking api

---
+++ Day2/FastAPI/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
+@app.get("/about")
+def about():
+    return {"message": "This is a FastAPI application."}
